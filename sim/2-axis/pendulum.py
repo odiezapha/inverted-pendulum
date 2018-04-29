996b1574b2bf58d44beaa8588acb8f958ed22967
Trent Fehl <trentonfehl@gmail.com>
2018-04-29 12:29:56 -0700

    added pendulum class

--- sim/2-axis/pendulum.py
+++ sim/2-axis/pendulum.py
@@ -11,9 +11,8 @@ import config_motor
 import config_wheel
 import config_pendulum
 
-class Controller():
+class Pendulum:
     def __init__(self, motor, wheel, pend):
-        ## Config ##
         self.Km = motor['Km']   # motor torque constant (Nm/A)
         self.Rm = motor['Rm']   # motor internal resistance (Ohms)
         self.Vsat = motor['Vsat']  # motor max voltage
@@ -36,6 +35,10 @@ class Controller():
 
         self.g = -9.81    # gravity (m/s^2)
 
+class Controller():
+    def __init__(self, Pendulum):
+        self.params = Pendulum.__dict__
+
     def get_system_state_matrix(self):
         """Return 9x9 system state matrix"""
 
@@ -47,24 +50,24 @@ class Controller():
 
         # X and Y gravity terms and Z rotation
         A[3:6,0:3] = matrix([
-            [2*self.g*self.lp*self.Mp/(self.Ix), 0, 0],
-            [0, 2*self.g*self.lp*self.Mp/(self.Iy), 0],
-            [0, 0, 2*self.g*self.Fspin_z/(self.Iz)]
+            [2*self.params['g']*self.params['lp']*self.params['Mp']/(self.params['Ix']), 0, 0],
+            [0, 2*self.params['g']*self.params['lp']*self.params['Mp']/(self.params['Iy']), 0],
+            [0, 0, 2*self.params['g']*self.params['Fspin_z']/(self.params['Iz'])]
         ])
         # Friction in pendulum rotation on pendulum
         A[3:6,3:6] = matrix([
-            [self.Bx/(self.Ix), 0, 0],
-            [0, self.By/(self.Iy), 0],
-            [0, 0, self.Bz/(self.Iz)]
+            [self.params['Bx']/(self.params['Ix']), 0, 0],
+            [0, self.params['By']/(self.params['Iy']), 0],
+            [0, 0, self.params['Bz']/(self.params['Iz'])]
         ])
         # Friction in wheel rotation on pendulum
         A[3:6,6:9] = matrix([
-            [-self.Bw/(self.Ix), 0, 0],
-            [0, -self.Bw/(self.Iy), 0],
-            [0, 0, -self.Bw/(self.Iz)]
+            [-self.params['Bw']/(self.params['Ix']), 0, 0],
+            [0, -self.params['Bw']/(self.params['Iy']), 0],
+            [0, 0, -self.params['Bw']/(self.params['Iz'])]
         ])
         # Friction in wheel rotation on wheel
-        A[6:9,6:9] = -self.Bw/self.Iw*identity(3)
+        A[6:9,6:9] = -self.params['Bw']/self.params['Iw']*identity(3)
 
         return A
 
@@ -76,12 +79,12 @@ class Controller():
 
         # control term impact on pendulum
         B[3:6,0:3] = matrix([
-            [self.Km/(self.Ix)/self.Rm, 0, 0],
-            [0, self.Km/(self.Iy)/self.Rm, 0],
-            [0, 0, self.Km/(self.Iz)/self.Rm]
+            [self.params['Km']/(self.params['Ix'])/self.params['Rm'], 0, 0],
+            [0, self.params['Km']/(self.params['Iy'])/self.params['Rm'], 0],
+            [0, 0, self.params['Km']/(self.params['Iz'])/self.params['Rm']]
         ])
         # control term impact on wheel
-        B[6:9,0:3] = self.Km/self.Iw/self.Rm*identity(3)
+        B[6:9,0:3] = self.params['Km']/self.params['Iw']/self.params['Rm']*identity(3)
 
         return B
 
@@ -129,41 +132,18 @@ class Controller():
         print(eigVecs)
 
 
-class Sim():
-    def __init__(self, motor, wheel, pend, K):
-        self.k = K
-        self.v = np.zeros(2) # initialize control voltages
+class Sim:
+    def __init__(self, Pendulum):
+        self.params = Pendulum.__dict__
+        self.v = np.zeros(3) # initialize control voltages
 
-        ## Config ##
-        self.Km = motor['Km']   # motor torque constant (Nm/A)
-        self.Rm = motor['Rm']   # motor internal resistance (Ohms)
-        self.Vsat = motor['Vsat']  # motor max voltage
-        self.Iw = wheel['Iw']   # wheel MOI (kg*m^2)
-        self.Bw = wheel['Bw']   # friction in wheel bearing (Nms)
-        self.Mr = pend['Mr']    # mass of rod (kg)
-        self.Yr = pend['Yr']    # normalized distance of rod along rod
-        self.Mw = pend['Mw']    # mass of wheel (kg)
-        self.Yw = pend['Yw']    # normalized distance of wheel along rod
-        self.lp = pend['lp']    # length of pendulum (m)
-        self.Ix = pend['Ix']    # pendulum MOI (kg*m^2)
-        self.Bx = pend['Bx']    # friction in pendulum bearing (Nms)
-        self.Iy = pend['Iy']    # pendulum MOI (kg*m^2)
-        self.By = pend['By']    # friction in pendulum bearing (Nms)
-        self.Iz = pend['Iz']    # pendulum MOI (kg*m^2)
-        self.Bz = pend['Bz']    # friction in pendulum bearing (Nms)
-        self.Fspin_z = pend['Fspin_z']  # force spinning around rod axis, term needed for stability
-
-        self.Mp = self.Mw*self.Yw + self.Mr*self.Yr # pendulum's weighted mass
-
-        self.g = -9.81    # gravity (m/s^2)
-
-    def deriv(self, u, t):
+    def deriv(self, u, t, K):
         """First order ODEs for integration"""
 
         # Generate control voltages, clip at saturation voltage
-        Vx = np.clip(float(-self.k[0]*matrix((u)).T), 0, self.Vsat)
-        Vy = np.clip(float(-self.k[1]*matrix((u)).T), 0, self.Vsat)
-        Vz = 0
+        self.v[0] = np.clip(float(-K[0]*matrix((u)).T), 0, self.params['Vsat'])
+        self.v[1] = np.clip(float(-K[1]*matrix((u)).T), 0, self.params['Vsat'])
+        self.v[2] = 0
 
         '''
         Definitions:
@@ -181,23 +161,33 @@ class Sim():
         x1_dt = 0.5*x4
         x2_dt = 0.5*x5
         x3_dt = 0.5*x6
-        x4_dt = self.Km*Vx/self.Rm/self.Ix + 2*self.g*self.lp*self.Mp/(self.Ix)*x1 + self.Bx/(self.Ix)*x4 - self.Bw/(self.Ix)*x7
-        x5_dt = self.Km*Vy/self.Rm/self.Iy + 2*self.g*self.lp*self.Mp/(self.Iy)*x2 + self.By/(self.Iy)*x5 - self.Bw/(self.Iy)*x8
-        x6_dt = self.Km*Vz/self.Rm/self.Iz + 2*self.g*self.Fspin_z/(self.Iz)*x3 + self.Bz/(self.Iz)*x6 - self.Bw/(self.Iz)*x9
-        x7_dt = self.Km*Vx/self.Rm/self.Iw + -self.Bw/self.Iw*x7
-        x8_dt = self.Km*Vy/self.Rm/self.Iw + -self.Bw/self.Iw*x8
-        x9_dt = self.Km*Vz/self.Rm/self.Iw + -self.Bw/self.Iw*x9
+        x4_dt = self.params['Km']*self.v[0]/self.params['Rm']/self.params['Ix'] \
+            + 2*self.params['g']*self.params['lp']*self.params['Mp']/(self.params['Ix'])*x1 \
+            + self.params['Bx']/(self.params['Ix'])*x4 - self.params['Bw']/(self.params['Ix'])*x7
+        x5_dt = self.params['Km']*self.v[1]/self.params['Rm']/self.params['Iy'] \
+            + 2*self.params['g']*self.params['lp']*self.params['Mp']/(self.params['Iy'])*x2 \
+            + self.params['By']/(self.params['Iy'])*x5 - self.params['Bw']/(self.params['Iy'])*x8
+        x6_dt = self.params['Km']*self.v[2]/self.params['Rm']/self.params['Iz'] \
+            + 2*self.params['g']*self.params['Fspin_z']/(self.params['Iz'])*x3 \
+            + self.params['Bz']/(self.params['Iz'])*x6 - self.params['Bw']/(self.params['Iz'])*x9
+        x7_dt = self.params['Km']*self.v[0]/self.params['Rm']/self.params['Iw'] \
+            + -self.params['Bw']/self.params['Iw']*x7
+        x8_dt = self.params['Km']*self.v[1]/self.params['Rm']/self.params['Iw'] \
+            + -self.params['Bw']/self.params['Iw']*x8
+        x9_dt = self.params['Km']*self.v[2]/self.params['Rm']/self.params['Iw'] \
+            + -self.params['Bw']/self.params['Iw']*x9
+
         dxdt = [x1_dt, x2_dt, x3_dt, x4_dt, x5_dt, x6_dt, x7_dt, x8_dt, x9_dt]
 
         return dxdt
 
-    def gen_data(self, system_init, dt, t_end):
+    def gen_data(self, system_init, dt, t_end, K):
         """ Use scipy.integrate.odeint to generate time series data"""
 
         x = system_init[:] # initialize system state
         t = np.arange(0.0, t_end, dt)
 
-        ts_data = odeint(self.deriv, x, t)
+        ts_data = odeint(self.deriv, x, t, args=(K,)) # extra arguments need to be a tuple
 
         return ts_data, t
 
@@ -225,7 +215,11 @@ def main():
     wheel = config_wheel.estimate
     pend = config_pendulum.estimate
 
-    C = Controller(motor, wheel, pend)
+    # Create pendulum object
+    pend = Pendulum(motor, wheel, pend)
+
+    # Create controller based on pendulum
+    C = Controller(pend)
 
     A = C.get_system_state_matrix()
     B = C.get_input_state_matrix()
@@ -234,8 +228,6 @@ def main():
 
     # Get LQR controller gains
     K = C.get_k_gain(A, B, Q, R)
-
-    # C.print_eigs(A, B, K)
 
     # Set up simulation
     duration = 15 # sec
@@ -251,10 +243,12 @@ def main():
             0.,
             0.]
 
-    S = Sim(motor, wheel, pend, K)
+    # Create sim of pendulum
+    S = Sim(pend)
 
-    ts_data, t = S.gen_data(sys_init, time_step, duration)
+    ts_data, t = S.gen_data(sys_init, time_step, duration, K)
 
+    # Plot data
     S.plot_timeseries(ts_data, t)
     S.plot_parametric(ts_data)
 
